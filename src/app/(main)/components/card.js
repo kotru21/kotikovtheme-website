969f447bd0efd29ce9e6ec110c6a58071d7cebfc
kotru21 <88907641+kotru21@users.noreply.github.com>
2023-07-12 20:19:42 +0300

    font color

--- src/app/(main)/components/card.js
+++ src/app/(main)/components/card.js
@@ -4,8 +4,8 @@ const Card = ({ title, description, image }) => {
   return (
     <div>
       <div className="px-6 py-6 ml-24 lg:backdrop-blur-none backdrop-blur-lg rounded">
-        <div className="font-bold text-xl mb-4 ">{title}</div>
-        <p className="text-gray-700 text-base ">{description}</p>
+        <div className="font-bold text-white text-xl mb-4 ">{title}</div>
+        <p className="text-gray-500 text-base ">{description}</p>
       </div>
     </div>
   );
